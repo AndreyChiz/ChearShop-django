8f3daf69f19c6dd8177351db48820aab773ae772
Andrey C <achizhovjob@yandex.ru>
2024-01-14 21:23:52 +0400

    pagination_paprams_not_links

--- goods/views.py
+++ goods/views.py
@@ -8,11 +8,21 @@ from goods.models import Products
 def catalog(request, category_slug):
 
     page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None) # если не будет параметра то None
+    order_by = request.GET.get('order_by', None) # аналогично
 
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+# фильтры
+    if on_sale: #если сработат то к goods применится
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != 'default': #если сработат то к goods применится
+        goods = goods.order_by(order_by)
+
 
     paginator = Paginator(goods, 3) # goods в любом случае проходит через пагинатор
     current_page = paginator.page(int(page))
